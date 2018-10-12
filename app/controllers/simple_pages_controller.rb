66b51e1c373229faa2c27e5830a3c8b668c8cc18
Ernest Ngiao <tengiao@gmail.com>
2018-10-12 13:49:00 +1300

    4.5 Display images dynamically in a carousel. Cleaned up views with partials.

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,6 +3,8 @@ class SimplePagesController < ApplicationController
   end
 
   def landing_page
-    @featured_product = Product.first
+    # @featured_product = Product.first
+    # @products = Product.limit(3)
+    @products = Product.all
   end
 end
