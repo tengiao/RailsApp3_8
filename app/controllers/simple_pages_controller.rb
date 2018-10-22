019396b8464dfab01647d35589db58d6159318d7
Ernest Ngiao <tengiao@gmail.com>
2018-10-22 15:58:55 +1300

    Integrated SendGrid plugin to Heroku.

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -7,4 +7,14 @@ class SimplePagesController < ApplicationController
     # @products = Product.limit(3)
     @products = Product.all
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: "tengiao@gmail.com",
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'simple_pages/landing_page'
   root 'simple_pages#landing_page'
   get 'simple_pages/index'
+  post 'simple_pages/thank_you'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
